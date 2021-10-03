373900be61c840573357eedced437f07127d801e
Azwad22 <mdazwad22@gmail.com>
2021-10-04 03:05:32 +0600

    2nd

--- ebay/models.py
+++ ebay/models.py
@@ -1,42 +1,44 @@
 from django.db import models
 
+
 class Admin(models.Model):
     a_id = models.AutoField(primary_key=True)
     aname = models.CharField(max_length=50)
     a_email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
 
-
     def __str__(self):
         return self.aname
 
 # Create your models here.
+
+
 class CUSTOMER(models.Model):
     c_id = models.AutoField(primary_key=True)
     cname = models.CharField(max_length=50)
     c_email = models.EmailField(max_length=50)
     password = models.CharField(max_length=50)
-    
+
     def __str__(self):
-        return str(self.c_id)+ " " +self.c_email
+        return str(self.c_id) + " " + self.c_email
 
 
 class PRODUCTS(models.Model):
     p_id = models.AutoField(primary_key=True)
     pname = models.CharField(max_length=50)
     p_des = models.TextField(max_length=250)
-    p_photo = models.ImageField(upload_to = 'images/')
+    p_photo = models.ImageField(upload_to='images/')
     endate = models.DateField()
     min_bid = models.CharField(max_length=20)
     owner = models.EmailField(max_length=50)
     winner = models.CharField(max_length=50, blank=True)
-      
+
     @staticmethod
     def get_all_products():
         return PRODUCTS.objects.all().order_by('-p_id')
 
     def __str__(self):
-        return str(self.p_id)+ " " +self.pname
+        return str(self.p_id) + " " + self.pname
 
 
 class BIDS(models.Model):
@@ -46,5 +48,9 @@ class BIDS(models.Model):
     bider = models.EmailField(max_length=50)
     price = models.CharField(max_length=20)
 
+    @staticmethod
+    def get_all_products():
+        return BIDS.objects.all().order_by('-Serial')
+
     def __str__(self):
-        return str(self.Serial)+ " " +str(self.pid)+ " " +self.p_name+ " " +self.bider
+        return str(self.Serial) + " " + str(self.pid) + " " + self.p_name + " " + self.bider
--- ebay/views.py
+++ ebay/views.py
@@ -54,7 +54,6 @@ def adminlogin(request):
 
             for i in Admin.objects.all():
                 if i.a_email == email_id and i.password == password:
-                    messages.success(request, 'successfull login')
                     return redirect('ebay:admindash')
 
     diction = {'title': "Admin Login", 'adminform': form}
@@ -63,12 +62,16 @@ def adminlogin(request):
 
 def adminDash(request):
     items = PRODUCTS.get_all_products()
-
-    # if request.GET:
-    #     searched = request.GET['search']
-    #     items = PRODUCTS.objects.filter(pname__contains=searched)
     diction = {'title': "Admin Dashboard", 'products': items}
     return render(request, 'ebay/admindash.html', context=diction)
+
+
+def bidder(request):
+    EMAIL = request.session['emailid']
+    bidder = BIDS.get_all_products()
+    diction = {'title': "Bidder Information",
+               'bidders': bidder, 'email': EMAIL}
+    return render(request, 'ebay/bidderinfo.html', context=diction)
 
 
 def register(request):
@@ -108,19 +111,20 @@ def addproducts(request):
     diction = {'title': "Add Product", 'addproductForm': form, 'email': EMAIL}
     return render(request, 'ebay/addproduct.html', context=diction)
 
-def update(request,pid):
+
+def update(request, pid):
     EMAIL = request.session['emailid']
     productinfo = PRODUCTS.objects.get(p_id=pid)
-    form = forms.addproductForm(instance= productinfo)
+    form = forms.addproductForm(instance=productinfo)
 
     if request.method == "POST":
-         form = forms.addproductForm(request.POST, instance=productinfo)
+        form = forms.addproductForm(request.POST, instance=productinfo)
 
-         if form.is_valid():
-             form.save(commit=True)
-             return redirect('ebay:admindash')
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('ebay:admindash')
 
-    diction = {'title':"Product Update", 'productinfo': form, 'email': EMAIL}
+    diction = {'title': "Product Update", 'productinfo': form, 'email': EMAIL}
     return render(request, 'ebay/productupdate.html', context=diction)
 
 
