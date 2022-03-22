1d58204e96285ed4afeec69bb779dc81ce161d52
Azwad22 <mdazwad22@gmail.com>
2022-03-23 03:24:08 +0600

    login

--- accounts/views.py
+++ accounts/views.py
@@ -24,7 +24,7 @@ from django.contrib.auth.tokens import PasswordResetTokenGenerator
 import requests
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+import re
 specialCharacters = ['$', '#', '@', '!', '*']
 
 
