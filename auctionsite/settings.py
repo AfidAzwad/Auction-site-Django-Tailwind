c32df86e93f3dfae2a5f3c6b874d011dd46a61e0
Azwad22 <mdazwad22@gmail.com>
2022-12-23 00:06:27 +0600

    Debug updated in settings

--- auctionsite/settings.py
+++ auctionsite/settings.py
@@ -18,9 +18,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['auctionsite-az.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
