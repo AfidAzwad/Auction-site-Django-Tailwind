dca35cb53e226a0ab3947c4ba1833b939025081a
Azwad22 <mdazwad22@gmail.com>
2022-12-23 17:12:24 +0600

    updated database settings

--- auctionsite/settings.py
+++ auctionsite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',
+                 'auctionsitedemo.up.railway.app']
 
 # Application definition
 
